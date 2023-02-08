390cc9899b5a743f0d2b99052071b8236403dbe1
Juwan <jhollingsworth5@student.clayton.edu>
2023-02-07 21:24:19 -0500

    implement top banner logic

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./banner.css";
+
+function Banner() {
+  return (
+    <div>
+      <div className="topbanner__line"></div>
+      <p className="topbanner__text">
+        Introducing ChatGPT research release Try Learn more
+      </p>
+    </div>
+  );
+}
+
+export default Banner;
