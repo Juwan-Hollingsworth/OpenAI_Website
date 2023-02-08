62298b211d5eca16722b6c708f74983abb604737
Juwan <jhollingsworth5@student.clayton.edu>
2023-02-07 21:15:47 -0500

    implement footer styling WIP

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -8,48 +8,50 @@ function Footer() {
         <svg xmlns="http://www.w3.org/2000/svg" />
       </div>
       <div class="footer__row">
-        <div class="column__A">
-          <h3>FEATURED</h3>
-          <ul class="featured__links">
-            <li>ChatGPT</li>
-            <li>DALL-E2</li>
-            <li>Whisper</li>
-            <li>Alignment</li>
-            <li>Startup Fund</li>
-          </ul>
-        </div>
-        <div class="column__B">
-          <h3>API</h3>
-          <ul class="api__links">
-            <li>Overview</li>
-            <li>Pricing</li>
-            <li>Examples</li>
-            <li>Docs</li>
-            <li>Terms & Policies</li>
-            <li>Status</li>
-            <li>Log in</li>
-          </ul>
-        </div>
-        <div class="column__C">
-          <h3>BLOG</h3>
-          <ul class="blog__links">
-            <li>Index</li>
-            <li>Research</li>
-            <li>Announcements</li>
-            <li>Events</li>
-            <li>Milestones</li>
-          </ul>
-        </div>
-        <div class="column__D">
-          <h3>INFROMATION</h3>
-          <ul class="information__links">
-            <li>About Us</li>
-            <li>Our Charter</li>
-            <li>Our Research</li>
-            <li>Publications</li>
-            <li>Newsroom</li>
-            <li>Careers</li>
-          </ul>
+        <div className="footer__links">
+          <div class="column__A">
+            <h3>FEATURED</h3>
+            <ul class="featured__links">
+              <li>ChatGPT</li>
+              <li>DALL-E2</li>
+              <li>Whisper</li>
+              <li>Alignment</li>
+              <li>Startup Fund</li>
+            </ul>
+          </div>
+          <div class="column__B">
+            <h3>API</h3>
+            <ul class="api__links">
+              <li>Overview</li>
+              <li>Pricing</li>
+              <li>Examples</li>
+              <li>Docs</li>
+              <li>Terms & Policies</li>
+              <li>Status</li>
+              <li>Log in</li>
+            </ul>
+          </div>
+          <div class="column__C">
+            <h3>BLOG</h3>
+            <ul class="blog__links">
+              <li>Index</li>
+              <li>Research</li>
+              <li>Announcements</li>
+              <li>Events</li>
+              <li>Milestones</li>
+            </ul>
+          </div>
+          <div class="column__D">
+            <h3>INFROMATION</h3>
+            <ul class="information__links">
+              <li>About Us</li>
+              <li>Our Charter</li>
+              <li>Our Research</li>
+              <li>Publications</li>
+              <li>Newsroom</li>
+              <li>Careers</li>
+            </ul>
+          </div>
         </div>
         <div className="footer__banner">
           <div class="row">
