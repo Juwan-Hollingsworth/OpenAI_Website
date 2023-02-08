c3bf6852141606c8be456f316ec44f7292556cb6
Juwan <jhollingsworth5@student.clayton.edu>
2023-02-07 21:18:27 -0500

    Import and style video in body container

--- src/containers/body/Body.jsx
+++ src/containers/body/Body.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./body.css";
+import video from "../../assets/hero__video.mp4";
+import { Banner } from "../../components";
 
 function Body() {
-  return <div>Body</div>;
+  return (
+    <div className="body__main">
+      <h2 className="hero__title">
+        Join us in shaping the future of technology.
+      </h2>
+      <video loop autoPlay={true} className="hero__video" src={video} muted />
+      <Banner />
+    </div>
+  );
 }
 
 export default Body;
