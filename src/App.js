db37d6aa3792a9d4c899cfa9c3541ea730dca0f3
Juwan <jhollingsworth5@student.clayton.edu>
2023-02-07 21:19:52 -0500

    restructure files + delete redundancies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Body, Footer, Header } from "./containers";
+import { Body, Footer } from "./containers";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      {/* <Header /> */}
       <Body />
       <Footer />
     </div>
